2acd886fb0e8dd6da3d3660a16d2ec97bc5af75d
un-rohit <un.rohitkumar@gmail.com>
2024-09-07 15:16:57 +0530

    piramind pattern
    printung pattren in cpp

--- pattern/Pyramind.cpp
+++ pattern/Pyramind.cpp
@@ -1,8 +1,30 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-    int n;
-    
-    return 0;
+    int n = 4;
+
+    for (int i = 0; i < n; i++)
+    {
+
+        // spaace
+        for (int j = 0; j < n- i-1; j++)
+        {
+            cout << " ";
+        }
+        // num1
+
+        for (int j = 1; j <= i + 1; j++)
+        {
+            cout <<j;
+        }
+
+        for (int j = i; j >=1; j--)
+        {
+            cout << j;
+        }
+    cout <<endl;
+    }
+
+        return 0;
 }
