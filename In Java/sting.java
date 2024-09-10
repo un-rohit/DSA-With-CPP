d6c25779155c7dd68cc5756dc282c421ffc00ff5
un-rohit <un.rohitkumar@gmail.com>
2024-09-10 23:03:48 +0530

    Looping

---
+++ In Java/sting.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class sting {
+    public static void main(String[] args) {
+        String name ="tony";
+        String fullname= " tony Stack";
+        String sen ="My name is tony stack.";
+
+    }
+}
