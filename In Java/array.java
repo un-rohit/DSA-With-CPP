b60fd9287c3a95027fbd68dad52585dcc912a0ad
un-rohit <un.rohitkumar@gmail.com>
2024-09-10 23:02:47 +0530

    Create array.java
    DSA in java

---
+++ In Java/array.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class array {
+    public static void main(String[] args) {
+        Scanner rk = new Scanner(System.in);
+        int[] a = { 1, 2, 3, 4, 5, 6 };
+        int b[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+        int c[] = new int[10];
+        
+        for (int i = 0; i < 10; i++)
+         {
+            c[i] = rk.nextInt();
+        }
+        System.out.println("hello WOrld!");
+        for()
+    }
+}
