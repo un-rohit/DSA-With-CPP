2bfe57f474a610ba8b9a708a0cc847bf729ce700
un-rohit <un.rohitkumar@gmail.com>
2024-09-21 15:10:30 +0530

    maxmin

--- newArrays/minMAx.cpp
+++ newArrays/minMAx.cpp
@@ -3,20 +3,25 @@
 using namespace std;
 int main()
 {
-    // int num[] = {4, 5, 45, 6, 7};
-    // int size = 5;
+    int num[] = {4, 5, 45, 1, 7};
+    int size = 5;
 
-    // int smallest = INT8_MAX;
+    int smallest = INT8_MAX;
+    int largest = INT8_MIN;
 
-    // for (int i = 0; i < size; i++)
-    // {
-    //     if (num[i] < smallest)
-    //     {
-    //         smallest = num[i];
-    //     }
-    // }
+    for (int i = 0; i < size; i++)
+    {
 
-    cout << "smallest = " << endl;
+        smallest=min(num[i],smallest);
+        largest=max(num[i],smallest);
+        // if (num[i] < smallest)
+        // {
+        //     smallest = num[i];
+        // }
+    }
+
+    cout << "Smallest = " << smallest<<endl;
+    cout<<" Largest = "<<largest;
 
     return 0;
 }
