45e1c509095e6e800170e5748679ccf827e174ce
un-rohit <un.rohitkumar@gmail.com>
2024-09-22 23:52:40 +0530

    8778

--- In Java/qn8/Question1.java
+++ In Java/qn8/Question1.java
@@ -1,56 +1,35 @@
-A nested class which is declared inside a block (between an opening and closing brace) is called a local class.
+In a Java class, the fields are normally initialized in the constructors. However, there is another way of initializing them in an unnamed block also called initializer block or a non-static initializer block.
 
-Inner classes are also local classes. However, to differentiate between inner and local classes, we use the term local class to a class which is declared inside a method or a conditional or a loop block. For example:
-class A { //this is called a top-level class
-	class B { //this is an inner class
-	}
-	static class C { //this is an inner class
-	}
-	public void someMethodInClassA() {
-		class D {
-		}
+For example, in the below code you will notice that the field value is initialized to 3 in an initializer block.
+
+Once the code is executed you will notice that the code snippets in the initializer blocks are executed (in the order in which they appear in the code) before the code in the constructor is executed. This is because the Java compiler copies all initializer blocks in the order they appear, into every constructor.
+
+Since we are creating two instances of class A using the new keyword, the constructor is called twice and during the constructor call first, all the initializer blocks are executed.
+
+If there is a block of code which has to be executed before the code inside any of the constructors is executed, such code can be placed in an initializer block.
+
+
+
+package q11289;
+public class NonStaticInitBlockDemo {
+	public static void main(String[] args) {
+		A a1 = new A();
+		A a2 = new A();
 	}
 }
-In the above code,
-A is called a top-level class
- B is called an inner class
-C is called a static nested class and
-D is called a local class
-Local classes can access instance members of their enclosing class.
-
-The visibility of the local class is restricted to the scope of the enclosing block (braces).
-
-Note that local classes cannot have static methods or interfaces as members. We can have fields marked as static provided they are also marked as final (such fields are called as constants).
-
-See and retype the below code.
-
-
-package q11296;
-public class A {
-	private int value = 7;
-	class B {
-		private B() {
-			System.out.println("In inner class B's constructor");
-		}
+class A {
+	private int value;
+	//below is an example of non-static initialization block
+	{
+		value = 3;
+		System.out.println("In non-static init block 1");
 	}
-	static class C {
-		private C() {
-			System.out.println("In static nested class C's constructor");
-		}
+	public A() {
+		System.out.println("In constructor");
+		System.out.println("value = " + value);
 	}
-	public void someMethodInClassA() {
-		class D {
-			private D() {
-				System.out.println("In local class D's constructor");
-				System.out.println("value = " + value);
-			}
-		}
-		D d = new D();
-	}
-	public static void main(String[] args) {
-		A a = new A();
-		A.B b = new A().new B();
-		A.C c = new A.C();
-		a.someMethodInClassA();
+	//below is an example of another non-static initialization block
+	{
+		System.out.println("In non-static init block 2");
 	}
 }
--- In Java/qn8/Question2.java
+++ In Java/qn8/Question2.java
@@ -1,60 +1,34 @@
-An anonymous class is very similar to a local class except that an anonymous class combines both the declaration and definition of the class into a single expression statement.
+Initializer blocks which are prefixed with the static keyword are called static initializer block.
 
-Secondly an anonymous class usually implements an interface or extends a class.
+Like the non-static initializer blocks, a class can have any number of static initializer blocks and they will be executed in the order in which they appear in the code.
 
-For example the below code shows the difference between an inner class and an anonymous class:
-interface Printer { //this is a top-level interface
-	public void printMe();
-}
-class A {  //this is a normal top-level class
+The code inside the non-static initalizer block is copied inside the constructor and executed during every invocation of the constructor.
+
+However, a static initializer block is executed only once when the class is loaded in memory. It has nothing to do with constructor invocation. Even if the constructor is not called to create an object instance, as long as the class is loaded the static initializer blocks get executed.
+
+Since the classes are loaded only once by the system class loader, these static initializer blocks get executed only once.
+
+See and retype the below code. On execution you will notice that code snippets in the static initializer blocks are executed only once even though we create two instances of class A.
+
+
+
+package q11290;
+public class StaticInitBlockDemo {
 	public static void main(String[] args) {
-		class PrinterImpl implements Printer { // an example of a normal local class
-			public void printMe() {
-				//do something....
-			}
-		}
-
-		Printer myPrinter1 = new PrinterImpl();
-
-		Printer myPrinter2 = new Printer() {// an example of an anonymous class 
-			public void printMe() {
-				//do something....
-			}
-		};
-		
-		myPrinter1.printMe();
-		myPrinter2.printMe();
+		A a1 = new A();
+		A a2 = new A();
 	}
 }
-In the above code, PrinterImpl is a local class that implements the interface Printer. An instance of this local class is created using the statement:
-Printer myPrinter1 = new PrinterImpl();
-The anonymous class which is initializing the reference myPrinter2 has the below properties:
-Anonymous classes do not have names
-An anonymous class is an expression containing a block of code which is terminated by a semicolon ;
-Anonymous class instantiation is done using the new keyword, just like a constructor invocation
-Anonymous class can either implement an interface and there by provide the implementation for its methods or extend a class and override required methods.
-When an anonymous class is implementing an interface, the new keyword is prefixed to the interface name, that is followed by empty parenthesis.
-When an anonymous class is extending a class, the new keyword is prefixed to one of the existing constructors in that class.
-See and retype the below code.
-
-package q11297;
-interface Printer {
-	public void printMe();
-}
 class A {
-	public static void main(String[] args) {
-		class PrinterImpl implements Printer {
-			public void printMe() {
-				System.out.println("I am in printMe method of the local class PrinterImpl instance");
-			}
-		}
-		Printer myPrinter1 = new PrinterImpl();
-		Printer myPrinter2 = new Printer() {
-			public void printMe() {
-				System.out.println("I am in printMe method of the anonymous class");
-			}
-		};
-		myPrinter1.printMe();
-		myPrinter2.printMe();
+	//below is an example of static initialization block
+	static {
+		System.out.println("In static init block 1");
+	}
+	public A() {
+		System.out.println("In constructor");
+	}
+	//below is an example of another static initialization block
+	static {
+		System.out.println("In static init block 2");
 	}
 }
