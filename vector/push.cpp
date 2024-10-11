db8f44c79edb535efde7628e4dc8b38d6630878b
un-rohit <un.rohitkumar@gmail.com>
2024-10-11 23:52:02 +0530

    k

--- vector/push.cpp
+++ vector/push.cpp
@@ -7,10 +7,20 @@ int main()
 
     cout << "Size of vector = " << vec.size()<<endl;
     vec.push_back(34);
-    cout << "After push Size of vector = " << vec.size();
+    vec.push_back(55);
+    vec.push_back(94);
+
+    cout << "After push Size of vector = " << vec.size()<<endl;
+    vec.pop_back();
+    // vec.front();
+    // vec.back();
        for (int val : vec) //  for each loop
     {
         cout << val << endl;
     }
+
+    cout<<endl<<"value in front "<<vec.front()<<endl;
+    cout<<"value in back "<<vec.back()<<endl;
+    cout<<"value at one "<<vec.at(0)<<endl;
     return 0;
 }
