94195a8c4f2ee6237a8842e78aae82626f4ca665
Rohit Kumar <un.rohitkumar@gmail.com>
2024-11-10 22:19:14 +0530

    j

---
+++ In Java/10nov/percenttage.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class percenttage {
+    public static void main(String[] args) {
+        Scanner mark= new Scanner(System.in);
+        System.out.println("Enter the no of Subject:");
+
+        int sub[] = mark.nextInt();
+        int got[sub] = mark.nextInt();
+        while (sub[] < 0) {
+
+            int sub[] = mark.nextInt();
+           int total=total+sub[i];
+        }
+       
+        System.out.println("PErcentage of all subject:"+ total/sub[i]);
+    }
+}
